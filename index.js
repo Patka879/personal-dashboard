c4f3b5e1c78c543ab3ecba001390da0cfd21c2b1
Patrycja Mysiak <mysiak.patrycja@gmail.com>
2022-08-17 22:48:08 +0200

    .catch method added for promises, default img set, connected to crypto currency api

--- index.js
+++ index.js
@@ -4,3 +4,16 @@ fetch('https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&que
         document.body.style.backgroundImage = `url(${data.urls.full})`
 		document.getElementById("author").textContent = `By: ${data.user.name}`
     })
+    .catch(err => {
+        document.body.style.backgroundImage = `url('./images/default.jpg')`  
+    })
+
+fetch('https://api.coingecko.com/api/v3/coins/bitcoin')
+    .then(res => res.json())
+    .then(data => {
+        document.getElementById("crypto-top").innerHTML = `
+        <img src=${data.image.small} />
+        <span>${data.name}</span>
+        `
+    })
+    .catch(err => console.log(err))
